42afeb0127225ce3ad789091fc6b68d4493d1ffb
josesito1237 <162739480+josesito1237@users.noreply.github.com>
2024-12-02 22:33:02 -0600

    Update app.js

--- app.js
+++ app.js
@@ -69,6 +69,8 @@ app.get('/acerca', (req, res) => {
     res.render('acerca'); // Renderiza la vista acerca
 });
 
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en el puerto ${PORT}`);
+});
 
-// Iniciar el servidor
-app.listen(3000, () => console.log('Servidor en http://localhost:3000'));
